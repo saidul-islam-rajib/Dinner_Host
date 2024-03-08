286bbbc8e949b46ef4d6299d5515685ee6217b61
Saidul Islam Rajib <saidul.bd@asa-international.com>
2024-03-08 14:27:00 +0600

    Global error handing using middleware

--- SoberDinner/SoberDinner.API/Program.cs
+++ SoberDinner/SoberDinner.API/Program.cs
@@ -1,3 +1,4 @@
+using SoberDinner.API.Middleware;
 using SoberDinner.Application.Services;
 using SoberDinner.Infrastructure.Services;
 
@@ -22,6 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 {
+    app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
