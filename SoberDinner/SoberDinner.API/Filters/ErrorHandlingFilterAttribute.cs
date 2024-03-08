fae74a43475a83504f2c73c184e1897ef07d5280
Saidul Islam Rajib <saidul.bd@asa-international.com>
2024-03-08 14:53:22 +0600

    Global error handling thrid and better approach

--- SoberDinner/SoberDinner.API/Filters/ErrorHandlingFilterAttribute.cs
+++ SoberDinner/SoberDinner.API/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
 
 namespace SoberDinner.API.Filters
 {
@@ -8,12 +9,18 @@ namespace SoberDinner.API.Filters
         public override void OnException(ExceptionContext context)
         {
             var exception = context.Exception;
-            context.Result = new ObjectResult(new { error = "An error occurred while precessing your request" })
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = "An error occurred while precessing your request",
+                Status = (int)HttpStatusCode.InternalServerError
+            };
+
+            context.Result = new ObjectResult(problemDetails)
             {
                 StatusCode = 500
             };
             context.ExceptionHandled = true;
-            base.OnException(context);
         }
     }
 }
