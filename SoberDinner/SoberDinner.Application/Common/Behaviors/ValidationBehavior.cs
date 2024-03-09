e321dd64d4af3873cf2ccc90126b5ed9fbbf7403
Saidul Islam Rajib <saidul.bd@asa-international.com>
2024-03-09 13:15:19 +0600

    Get error list

---
+++ SoberDinner/SoberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,39 @@
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+using SoberDinner.Application.Authentication.Commands.Register;
+using SoberDinner.Application.Authentication.Common;
+
+namespace SoberDinner.Application.Common.Behaviors
+{
+    public class ValidationRegisterCommandBehavior
+        : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
+    {
+        private readonly IValidator<RegisterCommand> _validator;
+
+        public ValidationRegisterCommandBehavior(IValidator<RegisterCommand> validator)
+        {
+            _validator = validator;
+        }
+
+        public async Task<ErrorOr<AuthenticationResult>> Handle(
+            RegisterCommand request,
+            RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+            CancellationToken cancellationToken)
+        {
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+            if (validationResult.IsValid)
+            {
+                return await next();
+            }
+
+            var errors = validationResult.Errors
+                .ConvertAll(validationFailure => Error.Validation(
+                    validationFailure.PropertyName,
+                    validationFailure.ErrorMessage));
+
+
+            return errors;
+        }
+    }
+}
--- SoberDinner/SoberDinner.Application/DependencyInjection.cs
+++ SoberDinner/SoberDinner.Application/DependencyInjection.cs
@@ -1,5 +1,11 @@
-﻿using MediatR;
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using SoberDinner.Application.Authentication.Commands.Register;
+using SoberDinner.Application.Authentication.Common;
+using SoberDinner.Application.Common.Behaviors;
+using System.Reflection;
 
 namespace SoberDinner.Application
 {
@@ -8,6 +14,12 @@ namespace SoberDinner.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+            services.AddScoped<
+                IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>,
+                ValidationRegisterCommandBehavior>();
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             return services;
         }
     }
