3c5f42f32f29c1b9aa77c89516b7cbac6fc513f1
Saidul Islam Rajib <saidul.bd@asa-international.com>
2024-03-10 15:30:52 +0600

    Add menu create controller

---
+++ SoberDinner/SoberDinner.API/Controllers/MenusController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoberDinner.Contracts.Menus;
+
+namespace SoberDinner.API.Controllers
+{
+    [Route("hosts/{hostId}/menus")]
+    public class MenusController : ApiController
+    {
+        [HttpPost]
+        public IActionResult CreateMenu(
+            CreateMenuRequest request,
+            string hostId)
+        {
+            return Ok(request);
+        }
+    }
+}
